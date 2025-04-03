f19e9e248ede3e1f92cb05033059b9955d8885f3
lafabricadeandroides <fernando.paniagua.python@gmail.com>
2025-04-03 08:15:23 +0200

    Update main

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ class Item(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/credits")
+def get_author():
+     return {"Nombre": "Fernando"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
